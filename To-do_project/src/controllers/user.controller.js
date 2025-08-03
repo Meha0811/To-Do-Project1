e61cb21dd659e62be33a63fa715bda22e1bae29d
Meha0811 <mehadangui242@gmail.com>
2025-08-04 02:15:24 +0530

    usercontroller created

--- To-do_project/src/controllers/user.controller.js
+++ To-do_project/src/controllers/user.controller.js
@@ -1,148 +1,71 @@
 const UserModel = require('../models/user.model');
-const HttpException = require('../utils/HttpException.utils');
-const { validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-dotenv.config();
 
-/******************************************************************************
- *                              User Controller
- ******************************************************************************/
-class UserController {
-    getAllUsers = async (req, res, next) => {
-        let userList = await UserModel.find();
-        if (!userList.length) {
-            throw new HttpException(404, 'Users not found');
-        }
+// Create a new user
+exports.createUser = async (req, res, next) => {
+  try {
+    const user = req.body;
 
-        userList = userList.map(user => {
-            const { password, ...userWithoutPassword } = user;
-            return userWithoutPassword;
-        });
-
-        res.send(userList);
-    };
-
-    getUserById = async (req, res, next) => {
-        const user = await UserModel.findOne({ id: req.params.id });
-        if (!user) {
-            throw new HttpException(404, 'User not found');
-        }
-
-        const { password, ...userWithoutPassword } = user;
-
-        res.send(userWithoutPassword);
-    };
-
-    getUserByuserName = async (req, res, next) => {
-        const user = await UserModel.findOne({ username: req.params.username });
-        if (!user) {
-            throw new HttpException(404, 'User not found');
-        }
-
-        const { password, ...userWithoutPassword } = user;
-
-        res.send(userWithoutPassword);
-    };
-
-    getCurrentUser = async (req, res, next) => {
-        const { password, ...userWithoutPassword } = req.currentUser;
-
-        res.send(userWithoutPassword);
-    };
-
-    createUser = async (req, res, next) => {
-        this.checkValidation(req);
-
-        await this.hashPassword(req);
-
-        const result = await UserModel.create(req.body);
-
-        if (!result) {
-            throw new HttpException(500, 'Something went wrong');
-        }
-
-        res.status(201).send('User was created!');
-    };
-
-    updateUser = async (req, res, next) => {
-        this.checkValidation(req);
-
-        await this.hashPassword(req);
-
-        const { confirm_password, ...restOfUpdates } = req.body;
-
-        // do the update query and get the result
-        // it can be partial edit
-        const result = await UserModel.update(restOfUpdates, req.params.id);
-
-        if (!result) {
-            throw new HttpException(404, 'Something went wrong');
-        }
-
-        const { affectedRows, changedRows, info } = result;
-
-        const message = !affectedRows ? 'User not found' :
-            affectedRows && changedRows ? 'User updated successfully' : 'Updated faild';
-
-        res.send({ message, info });
-    };
-
-    deleteUser = async (req, res, next) => {
-        const result = await UserModel.delete(req.params.id);
-        if (!result) {
-            throw new HttpException(404, 'User not found');
-        }
-        res.send('User has been deleted');
-    };
-
-    userLogin = async (req, res, next) => {
-        this.checkValidation(req);
-
-        const { email, password: pass } = req.body;
-
-        const user = await UserModel.findOne({ email });
-
-        if (!user) {
-            throw new HttpException(401, 'Unable to login!');
-        }
-
-        const isMatch = await bcrypt.compare(pass, user.password);
-
-        if (!isMatch) {
-            throw new HttpException(401, 'Incorrect password!');
-        }
-
-        // user matched!
-        const secretKey = process.env.SECRET_JWT || "";
-        const token = jwt.sign({ user_id: user.id.toString() }, secretKey, {
-            expiresIn: '24h'
-        });
-
-        const { password, ...userWithoutPassword } = user;
-
-        res.send({ ...userWithoutPassword, token });
-    };
-
-    checkValidation = (req) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            throw new HttpException(400, 'Validation faild', errors);
-        }
+    if (!user.name || !user.email) {
+      return res.status(400).json({ message: 'Name and email are required' });
     }
 
-    // hash password if it exists
-    hashPassword = async (req) => {
-        if (req.body.password) {
-            req.body.password = await bcrypt.hash(req.body.password, 8);
-        }
+    const result = await UserModel.createUser(user);
+    res.status(201).json({
+      message: 'User created successfully',
+      userId: result.insertId
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get all users
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await UserModel.getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get user by ID
+exports.getUserById = async (req, res, next) => {
+  try {
+    const user = await UserModel.getUserById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
-}
 
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
+// Update a user
+exports.updateUser = async (req, res, next) => {
+  try {
+    const updatedData = req.body;
 
-/******************************************************************************
- *                               Export
- ******************************************************************************/
-module.exports = new UserController;
+    if (!updatedData.name || !updatedData.email) {
+      return res.status(400).json({ message: 'Name and email are required' });
+    }
+
+    const result = await UserModel.updateUser(req.params.id, updatedData);
+    res.status(200).json({ message: 'User updated successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete a user
+exports.deleteUser = async (req, res, next) => {
+  try {
+    await UserModel.deleteUser(req.params.id);
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
