e90514e2b9c751c67cb1131fcf3d388230d30686
Meha0811 <mehadangui242@gmail.com>
2025-08-05 02:53:12 +0530

    uservalidator update

--- To-do_project/src/middleware/validators/userValidator.middleware.js
+++ To-do_project/src/middleware/validators/userValidator.middleware.js
@@ -2,28 +2,26 @@ const { body } = require('express-validator');
 
 exports.createUserValidator = [
   body('name')
-    .notEmpty().withMessage('Name is required')
-    .isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
+    .exists()
+    .withMessage('Name is required')
+    .isLength({ min: 3 })
+    .withMessage('Name must be at least 3 characters long'),
 
   body('email')
-    .notEmpty().withMessage('Email is required')
-    .isEmail().withMessage('Must be a valid email')
-    .normalizeEmail()
+    .exists()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Invalid email format'),
 ];
 
 exports.updateUserValidator = [
   body('name')
     .optional()
-    .isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
+    .isLength({ min: 3 })
+    .withMessage('Name must be at least 3 characters long'),
 
   body('email')
     .optional()
-    .isEmail().withMessage('Must be a valid email')
-    .normalizeEmail(),
-
-  body().custom(body => {
-    const allowedFields = ['name', 'email'];
-    const fields = Object.keys(body);
-    return fields.every(field => allowedFields.includes(field));
-  }).withMessage('Invalid fields in update request')
+    .isEmail()
+    .withMessage('Invalid email format'),
 ];
